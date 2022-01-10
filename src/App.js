f5abce3ac371d0edd423f4b65d2aae99a6b61e12
Ahmed <ahmedreda152@gmail.com>
2022-01-10 15:11:39 +0200

    implement todo List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-function App() {
+import { useEffect, useState } from 'react';
+import Header from './Components/TodoHeader';
+import TaskInput from './Components/TodoInput';
+import TodoRow from './Components/TodoRow';
+import { getData, sendData } from './utils/localStorage';
+
+const App = () => {
+  const [todoTasks, setTodoTasks] = useState([]);
+  console.log(todoTasks);
+  const data = getData();
+  useEffect(() => {
+    setTodoTasks(data);
+  }, []);
+
+  const addTask = (task) => {
+    if (!task.text) {
+      return;
+    }
+    const newTasks = [task, ...todoTasks];
+    sendData(newTasks);
+    setTodoTasks(newTasks);
+  };
+
+  const deleteTask = (taskID) => {
+    const filterdTasks = todoTasks.filter((task) => task.id !== taskID);
+    sendData(filterdTasks);
+    setTodoTasks(filterdTasks);
+  };
+
+  const changeTaskStatus = (taskID) => {
+    const updated = todoTasks.map((todo) => {
+      if (todo.id === taskID) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+    sendData(updated);
+    setTodoTasks(updated);
+  };
+
   return (
-    <div className="App">
-      <h1 className="text-5xl text-red-600">Hello</h1>
+    <div className="w-screen h-screen bg-slate-200 items-center justify-center flex">
+      <div className="flex items-center bg-white flex-col w-3/5 rounded-2xl m-auto shadow-2xl">
+        <Header />
+        <TaskInput onSubmit={addTask} />
+        {data.length
+          ? data?.map((todo) => (
+              <TodoRow
+                key={todo.id}
+                task={todo}
+                deleteOne={deleteTask}
+                changeStatus={changeTaskStatus}
+              />
+            ))
+          : 'no items added yet'}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/TodoRow.jsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+const TodoRow = ({ task, deleteOne, changeStatus }) => {
+  const { id, text } = task;
+
+  return (
+    <div className="w-full h-[80px] grid grid-cols-8 items-center px-5">
+      <div className="w-full flex">
+        <input
+          type="checkbox"
+          name="status"
+          id="status"
+          checked={true ? task.done === true : false}
+          onChange={() => {
+            changeStatus(id);
+          }}
+          className="cursor-pointer"
+        />
+      </div>
+      <p className="text-lg col-span-6">{text}</p>
+      <div className="w-full flex justify-between px-5">
+        <FontAwesomeIcon
+          name={id}
+          icon={faTrash}
+          className="text-2xl cursor-pointer"
+          onClick={() => deleteOne(id)}
+        />
+      </div>
+    </div>
+  );
+};
+export default TodoRow;
